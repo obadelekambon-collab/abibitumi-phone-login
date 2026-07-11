3c82977e19fa2c3e904a612312f7d8a31f722d9a
obadelekambon-collab <290356264+obadelekambon-collab@users.noreply.github.com>
2026-07-11 15:33:51 +0000

    Use matching WordPress 5.8 test framework

--- abibitumi-chat/tests/integration/bootstrap.php
+++ abibitumi-chat/tests/integration/bootstrap.php
@@ -9,7 +9,8 @@ require_once $plugin_dir . '/vendor/autoload.php';
 define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', $plugin_dir . '/vendor/yoast/phpunit-polyfills' );
 putenv( 'WP_PHPUNIT__TESTS_CONFIG=' . __DIR__ . '/wp-tests-config.php' );
 
-$tests_dir = $plugin_dir . '/vendor/wp-phpunit/wp-phpunit';
+$tests_dir = getenv( 'WP_TESTS_FRAMEWORK_DIR' );
+$tests_dir = $tests_dir ? $tests_dir : $plugin_dir . '/vendor/wp-phpunit/wp-phpunit';
 require_once $tests_dir . '/includes/functions.php';
 
 tests_add_filter(
